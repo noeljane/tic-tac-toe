a899814684a30b4c3dc6308c71c9352a9e9a3b2b
Philippe Luchansky <philuchansky@gmail.com>
2015-06-26 15:38:10 -0700

    add event listeners for all grid units

--- js/app.js
+++ js/app.js
@@ -1,1 +1,9 @@
-console.log("I'm here");
+var board = document.querySelectorAll('.grid-unit');
+
+function someFunction(){
+	console.log(this);
+}
+
+for(i = 0; i < board.length; i ++) {
+	board[i].addEventListener('click', someFunction);
+}
