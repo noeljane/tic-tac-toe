09f2eca59efd169f2b1f70c7553e49555566038d
Philippe Luchansky <philuchansky@gmail.com>
2015-06-27 13:58:29 -0700

    added player objects markPlayed refers to player object instead of className announceWinner() function lockBoard() function checkForWinner() cleanup playMove() refactored whoseTurn variable toggles player objects instead of boolean

--- js/app.js
+++ js/app.js
@@ -1,6 +1,9 @@
-//it's player 1's turn
-var p1Turn = true;
-var plays = 0;
+//create player objects
+var player1 = {id: 'p1', name: 'Player 1'};
+var player2 = {id: 'p2', name: 'Player 2'};
+
+//start with player1's turn
+var whoseTurn = player1;
 
 var clearBtn = document.querySelector('#clear');
 
@@ -10,79 +13,81 @@ var gridCount = board.length;
 
 //square is played
 function markPlayed(what,who) {
-	what.p = who;
-	what.className = 'played';
-	what.className += who;
+	what.p = who.id;
+	what.className = 'played by ';
+	what.className += who.id;
 
-	console.log(what + " was played by " + what.p);
+	console.log(what + " was played by " + who.name);
 }
 
 //Announce Winner
-function getWinner() {
-	//push moves to temporary array:
-/*	var playArray = [];
-	for(var i = 0; i < board.length; i ++) {
-		if(board[i].classList.contains('p1')) {
-			playArray.push('1');
-		} else {
-			playArray.push('2');
-		}
-	}*/
+function announceWinner(player) {
+	console.log(player + ' is the WINNER!');
 }
 
-
+//lock the board
+function lockBoard(){
+	for(var i = 0; i < board.length; i ++) {
+		board[i].removeEventListener('click', playMove);
+	}
+}
 
 function checkForWinner() {
-	if((board[0].p == board[1].p && board[0].p == board[2].p) || (board[3].p == board[4].p && board[3].p == board[5].p) || board[6].p == board[7].p && board[6].p == board[8].p) { //check rows
-		console.log('WINNER!');
-	} else if((board[0].p == board[3].p && board[0].p == board[6].p) || (board[1].p == board[4].p && board[1].p == board[7].p) || (board[2].p == board[5].p && board[2].p == board[8].p)) { //check columns
-		console.log('WINNER!');
-	} else if((board[0].p == board[4].p && board[0].p == board[8].p) || (board[2].p == board[4].p && board[2].p == board[6].p)) { //check diagonals
-		console.log('WINNER!');
+	//check rows
+	if((board[0].p == board[1].p && board[0].p == board[2].p) || (board[3].p == board[4].p && board[3].p == board[5].p) || board[6].p == board[7].p && board[6].p == board[8].p) {
+		announceWinner(whoseTurn.name);
+		lockBoard();
+	}
+	//check columns
+	else if((board[0].p == board[3].p && board[0].p == board[6].p) || (board[1].p == board[4].p && board[1].p == board[7].p) || (board[2].p == board[5].p && board[2].p == board[8].p)) {
+		announceWinner(whoseTurn.name);
+		lockBoard();
+	}
+	//check diagonals
+	else if((board[0].p == board[4].p && board[0].p == board[8].p) || (board[2].p == board[4].p && board[2].p == board[6].p)) {
+		announceWinner(whoseTurn.name);
+		lockBoard();
+	}
+	//check cats cradle
+	else if(plays == gridCount) {
+		console.log("It's a tie!");
 	}
 }
 
 //make a move
 function playMove(){
 	if(!this.classList.contains('played')){
-		if(p1Turn) {
+		markPlayed(this, whoseTurn);
+
+		//add 1 to number of plays
+		plays ++;
+		checkForWinner();
+
+		if(whoseTurn == player1) {
 			this.innerHTML = "<i class='fa fa-times'></i>";
-			p1Turn = false;
-			markPlayed(this, ' p1');
+			whoseTurn = player2;
+
 		} else {
 			this.innerHTML = "<i class='fa fa-circle-o'></i>";
-			p1Turn = true;
-			markPlayed(this, ' p2');
-		}
-
-		//add 1 to plays
-		plays ++;
-
-		//check if max 
-		if(plays == gridCount){
-			console.log("Game Over");
-			getWinner();
-		} else {
-			//console.log(this.classList);
+			whoseTurn = player1;
 		}
 	}
-
-	checkForWinner();
 }
 
 function initBoard() {
 	for(var i = 0; i < board.length; i ++) {
+		//reset board and square classes to ""
 		board[i].className = "";
 		board[i].innerHTML = "";
 
 		//add click listeners to all squares:
 		board[i].addEventListener('click', playMove);
+
 		//make sure none of the board[i].p are equivalent:
 		board[i].p = i;
-		
 	}
 	plays = 0;
-	p1Turn = true;
+	whoseTurn = player1;
 }
 
 clearBtn.addEventListener('click', initBoard);
