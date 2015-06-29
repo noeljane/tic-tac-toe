ffec2148885c93bb9d0967a385b4351bfc559584
Philippe Luchansky <philuchansky@gmail.com>
2015-06-29 00:26:30 -0700

    line breaks and comments

--- js/app.js
+++ js/app.js
@@ -13,11 +13,11 @@ for(i = 0; i < 9; i ++){
 var squares = document.querySelectorAll('#board div');
 var gridCount = squares.length;
 
-
-//create player objects, so that in the future, players can type in their names and make a name association in-game.
+//create player objects, with id and name properties.
 var player1 = {id: 'p1', name: 'Player 1'};
 var player2 = {id: 'p2', name: 'Player 2'};
 
+//create empty winner / loser objects for use in announceWinner()
 var winner = null;
 var loser = null;
 
@@ -43,10 +43,14 @@ function playMove(){
 
 //square is played
 function markPlayed(what,who) {
+	//assign the current player's id as the p property of the square that was just played
 	what.p = who.id;
+
+	//add HTML class accordingly
 	what.className = 'played by ';
 	what.className += who.id;
 
+	//add an 'X' or 'O' to the square
 	if(who == player1) {
 		what.innerHTML = xMark;
 	} else {
@@ -62,34 +66,35 @@ function markPlayed(what,who) {
 
 
 function checkForWinner() {
-	//check rows
+	//check rows for a winner
 	if((squares[0].p == squares[1].p && squares[0].p == squares[2].p) || (squares[3].p == squares[4].p && squares[3].p == squares[5].p) || squares[6].p == squares[7].p && squares[6].p == squares[8].p) {
 		announceWinner(whoseTurn);
 	}
-	//check columns
+	//check columns for a winner
 	else if((squares[0].p == squares[3].p && squares[0].p == squares[6].p) || (squares[1].p == squares[4].p && squares[1].p == squares[7].p) || (squares[2].p == squares[5].p && squares[2].p == squares[8].p)) {
 		announceWinner(whoseTurn);
 	}
-	//check diagonals
+	//check diagonals for a winner
 	else if((squares[0].p == squares[4].p && squares[0].p == squares[8].p) || (squares[2].p == squares[4].p && squares[2].p == squares[6].p)) {
 		announceWinner(whoseTurn);
 	}
-	//check cats cradle
+	//check for tie game
 	else if(plays == gridCount) {
 		announceTie();
 	}
+	//toggle player turn if the game should keep going
 	else {
 		if(whoseTurn == player1) {
 			whoseTurn = player2;
 		} else {
 			whoseTurn = player1;
 		}
-		console.log(whoseTurn.name + "'s turn.");
 	}
 }
 
 //Announce Winner
 function announceWinner(player) {
+	//Change #title to read winning player
 	winner = player;
 	h1.textContent = (winner.name + ' wins!');
 
@@ -100,10 +105,10 @@ function announceWinner(player) {
 		loser = player1;
 	}
 
+	//Loop through squares and attach winner/loser classes to each
 	for(i = 0; i < squares.length; i ++) {
 		if(squares[i].classList.contains(winner.id)){
 			squares[i].firstChild.className += ' winner';
-			//console.log(squares[i].firstChild);
 		} else if(squares[i].classList.contains(loser.id)) {
 			squares[i].firstChild.className += ' loser';
 		}
@@ -117,10 +122,7 @@ function announceTie() {
 	lockBoard();
 }
 
-
-
-
-//lock the board
+//Lock the board
 function lockBoard(){
 	for(var i = 0; i < gridCount; i ++) {
 		squares[i].removeEventListener('click', playMove);
@@ -129,7 +131,7 @@ function lockBoard(){
 	board.className += 'locked';
 }
 
-//clear board, reset
+//Clear board, reset
 function initTicTacToe() {
 
 	//reset h1 to default gameName text:
@@ -141,8 +143,8 @@ function initTicTacToe() {
 	//stop animation class from clear button:
 	clearBtn.className = "";
 
+	//reset board and square classes to ""
 	for(var i = 0; i < gridCount; i ++) {
-		//reset board and square classes to ""
 		squares[i].className = "";
 		squares[i].innerHTML = "";
 		//add click listeners to all squares:
@@ -150,6 +152,8 @@ function initTicTacToe() {
 		//make sure none of the board[i].p are equivalent:
 		squares[i].p = i;
 	}
+
+	//Reset play count, player turn to Player 1, and reset clear btn
 	plays = 0;
 	whoseTurn = player1;
 	clearBtn.addEventListener('click', initTicTacToe);
